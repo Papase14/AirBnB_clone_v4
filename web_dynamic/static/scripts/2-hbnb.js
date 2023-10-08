3fd5f0883c4c1c7f56ee1cd137322ebecaff4c9d
Tshupane Morake <tshupane@outlook.com>
2023-10-08 15:09:21 +0200

    fixed list

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,52 +1,23 @@
-$(document).ready(function() {
-    // Initialize an empty dictionary to store Amenity IDs
-    const amenityIDs = {};
-
-    // Function to update the amenities h4 tag
-    function updateAmenitiesList() {
-        const amenitiesList = Object.values(amenityIDs).join(', ');
-        $('.amenities h4').text(amenitiesList);
-    }
-
-    // Function to check the API status
-    function checkAPIStatus() {
-        $.ajax({
-            type: 'GET',
-            url: 'http://0.0.0.0:5001/api/v1/status/',
-            success: function(response) {
-                if (response.status === 'OK') {
-                    // API is available, add the 'available' class
-                    $('#api_status').addClass('available');
-                } else {
-                    // API is not available, remove the 'available' class
-                    $('#api_status').removeClass('available');
-                }
-            },
-            error: function() {
-                // Handle errors here if needed
-                console.error('Error checking API status');
-            }
-        });
-    }
-
-    // Initial check of API status
-    checkAPIStatus();
-
-    // Listen for changes on each input checkbox
-    $('input[type="checkbox"]').change(function() {
-        const checkbox = $(this);
-        const amenityID = checkbox.data('amenity-id');
-        const amenityName = checkbox.data('amenity-name');
-
-        if (checkbox.prop('checked')) {
-            // Checkbox is checked, add Amenity ID to the dictionary
-            amenityIDs[amenityID] = amenityName;
+window.addEventListener('load', function () {
+    $.ajax('http://0.0.0.0:5001/api/v1/status').done(function (data) {
+        if (data.status === 'OK') {
+            $('#api_status').addClass('available');
         } else {
-            // Checkbox is unchecked, remove Amenity ID from the dictionary
-            delete amenityIDs[amenityID];
+            $('#api_status').removeClass('available');
         }
+    });
 
-        // Update the h4 tag with the list of checked Amenities
-        updateAmenitiesList();
+    const amenityIds = {};
+    $('input[type=checkbox]').click(function () {
+        if ($(this).prop('checked')) {
+            amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
+        } else if (!$(this).prop('checked')) {
+            delete amenityIds[$(this).attr('data-id')];
+        }
+        if (Object.keys(amenityIds).length === 0) {
+            $('div.amenities h4').html('&nbsp');
+        } else {
+            $('div.amenities h4').text(Object.values(amenityIds).join(', '));
+        }
     });
 });
